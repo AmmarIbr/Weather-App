a06ca0e987b653dcf36b13b8b45744bdae4994bc
AmmarIbr <119716484+AmmarIbr@users.noreply.github.com>
2023-03-13 06:59:30 -0400

    Added 'current weather' data from API

--- app.js
+++ app.js
@@ -2,36 +2,17 @@
 const searchBox = document.querySelector(".search");
 const searchSection = document.querySelector(".autoCompleteBox");
 let currentTime = document.querySelector("#currentTime");
-console.log(searchSection);
+const form = document.querySelector(".searchForm");
 let query = "";
-/*
-async function autoComplete(e) {
-  if (query.length <= 3 || query === "") {
-    query = searchBox.value;
-  } else {
-    query = searchBox.value;
-    console.log(query);
-    const config = { params: { q: query } };
-    const res = await axios.get(
-      `https://api.weatherapi.com/v1/search.json?key=30832b5a2a13422485f64334231003`,
-      config
-    );
-    for (let i = 0; i < searchResult.length; i++) {
-      if (res.data[i] === undefined) {
-        break;
-      } else if (query.length < 4) {
-        searchResult[i].classList.remove("active");
-      } else {
-        searchResult[
-          i
-        ].textContent = `${res.data[i].name}, ${res.data[i].region}, ${res.data[i].country}`;
-        searchResult[i].classList.add("active");
-        console.log(res.data);
-      }
-    }
-  }
-}
-*/
+
+fetch("https://www.weatherapi.com/docs/weather_conditions.json")
+  .then((res) => res.json())
+  .then((out) => {
+    const allWeather = out;
+    // console.log("Output: ", allWeather);
+  })
+  .catch((err) => console.error(err));
+
 async function autoComplete(e) {
   let searchResult = document.querySelectorAll(".searchResult");
   searchResult.forEach((element) => {
@@ -82,6 +63,10 @@ function selectItem(e) {
   searchResult.forEach((element) => {
     element.remove();
   });
+
+  getCurrentWeather();
+  searchBox.value = "";
+  // form.submit();
   //   console.log(e);
   //   console.log(searchBox.value);
 }
@@ -100,8 +85,50 @@ function getCurrentTime() {
   } else {
     amOrpm = "AM";
   }
+  if (min < 10) {
+    min = `0${min}`;
+  }
   currentTime.textContent = `${hour}:${min} ${amOrpm}`;
 }
+
+async function getCurrentWeather(e) {
+  // e.preventDefault();
+  console.log(searchBox.value);
+  const config = { params: { q: searchBox.value } };
+  const res = await axios.get(
+    `https://api.weatherapi.com/v1/current.json?key=30832b5a2a13422485f64334231003`,
+    config
+  );
+  let dayOrNight = isDay(res.data.current);
+  console.log(dayOrNight);
+  console.log(res.data.current);
+  setCurrentWeather(res.data.current);
+}
+
+function setCurrentWeather(currentWeather) {
+  const displayTemp = document.querySelector(".displayTemp");
+  const celsius = document.createElement("span");
+  const feel = document.querySelector(".feelsLike");
+  const condition = document.querySelector(".condition");
+  displayTemp.innerText = `${currentWeather.temp_c}°`;
+  celsius.textContent = "C";
+  celsius.classList.add("celsius");
+  displayTemp.appendChild(celsius);
+  // celsius.textContent = "C";
+  feel.textContent = `Feels Like ${currentWeather.feelslike_c}°`;
+  condition.textContent = currentWeather.condition.text;
+  console.dir(celsius);
+}
+
+function isDay(info) {
+  if (info.is_day === 1) {
+    return "day";
+  } else {
+    return "night";
+  }
+}
+
 addEventListener("DOMContentLoaded", getCurrentTime);
 searchBox.addEventListener("input", autoComplete);
 searchSection.addEventListener("click", selectItem);
+// form.addEventListener("submit", getCurrentWeather);
